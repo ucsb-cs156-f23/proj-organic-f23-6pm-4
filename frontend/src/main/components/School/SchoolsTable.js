08cb35d25618858623847f6e19cdd32a71f9d177
Chelsea <chelseamstoner@gmail.com>
2023-11-29 11:59:51 -0800

    cs - fixed bug preventing error from rendering

--- frontend/src/main/components/School/SchoolsTable.js
+++ frontend/src/main/components/School/SchoolsTable.js
@@ -48,7 +48,7 @@ export default function SchoolsTable({
         },
         {
             Header: 'Term Error',
-            id: 'termError',
+            accessor: 'termError',
         }
     ];
 
